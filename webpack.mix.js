0ed6b4176f7d185c516a2f45435e316d808ba625
mattiagiaco <martex781227@gmail.com>
2022-02-10 17:01:53 +0100

    fix admin

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/admin.js', 'public/js')
     .js('resources/js/guest.js', 'public/js')
     .sass('resources/sass/admin/style.scss', 'public/css/admin')
-    .sass('resources/sass/guest/Style.scss', 'public/css/guest')
+    .sass('resources/sass/guest/style.scss', 'public/css/guest')
     .options({
         processCssUrl: false,
     });
